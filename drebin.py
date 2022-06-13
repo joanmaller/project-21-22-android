1c26460cb4ba26a79feccc9794b9925cac46fe2b
YouSeemConfused <o.filali@studenti.unica.it>
2022-06-13 16:25:02 +0200

    Call analyzer on multiple files

--- drebin.py
+++ drebin.py
@@ -1,4 +1,17 @@
 import staticAnalyzer
 import sys
+import os
+import settings
 
-staticAnalyzer.run(sys.argv[1], sys.argv[2])
+
+#usage: python drebin.py .
+apk_dir = settings.GOOD_APK_DIR
+
+for file in os.listdir(apk_dir):
+
+    if file.endswith("apk"):
+        full_path_apk = apk_dir + file
+        print("\n[I]\t", "Extracting data from", file)
+        staticAnalyzer.run(full_path_apk, sys.argv[1])
+    else:
+        print("\n[I]\t", file, "not an APK...")
