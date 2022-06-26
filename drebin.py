05648e0e937ba558941ebde1630978786e8ccd1f
YouSeemConfused <o.filali@studenti.unica.it>
2022-06-26 10:53:17 +0200

    Minor improvements
    Fix mismatch variable name
    Improve verbosity

--- drebin.py
+++ drebin.py
@@ -7,11 +7,16 @@ import settings
 #usage: python drebin.py .
 dirs = [settings.GOOD_APK_DIR, settings.BAD_APK_DIR]
 
-for dir in dirs:
-  for file in os.listdir(dir):
+for cur_dir in dirs:
+  count = 0
+  dir_total = len(os.listdir(cur_dir))
+
+  for file in os.listdir(cur_dir):
+      print(cur_dir, ": ", count, "of", dir_total)
+      count += 1
 
       if file.endswith("apk"):
-          full_path_apk = apk_dir + file
+          full_path_apk = cur_dir + file
           print("\n[I]\t", "Extracting data from", file)
           staticAnalyzer.run(full_path_apk, sys.argv[1])
       else:
